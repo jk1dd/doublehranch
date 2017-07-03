31c84e6ae0aa94bfb2dadf1ab05e92172e057ce0
Casa Maia <casamaia@Gabriels-iMac.local>
2017-07-03 11:30:59 -0400

    Add Paperclip config to production.rb
    ***Why is this change necessary?***
    Users are unable to upload photos because the path for Paperclip is not
    known.
    
    ***How does this accomplish the change?***
    Add paths for Paperclip to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,10 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  # Paperclip config:
+  Paperclip.options[:image_magick_path] = "/app/vendor/ImageMagick/bin"
+  Paperclip.options[:command_path] = "/app/vendor/ImageMagick/bin"
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
